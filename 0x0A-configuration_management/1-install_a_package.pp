1e9c4348c18dfe17bf0105e9ae882069b0605f23
sulaimonwasiu <sulaimonwasiu13@gmail.com>
2024-05-08 07:12:02 -0700

    Instal Flask via puppet

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,9 +1,6 @@
-class { 'python::pip': }
-
-package { 'python3-pip': ensure => installed, provider => 'apt' }
+# Using Puppet, install flask
 
 package { 'flask':
   ensure   => '2.1.0',
-  provider => 'pipx',
-  require  => Class['python::pip'],
+  provider => 'pip3'
 }
